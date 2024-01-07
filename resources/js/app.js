edec0ea290c5f2611defe77cb8b8acfa539f5778
Ximo Pedra Lindo <ximopedra@gmail.com>
2024-01-07 19:23:34 +0100

    Se ha quitado swup.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,4 @@
-// app entrypoint
-import Swup from "swup";
-import SwupHeadPlugin from '@swup/head-plugin';
-import SwupFormsPlugin from '@swup/forms-plugin';
 import Chart from 'chart.js/auto';
-const swup = new Swup({
-    plugins: [
-        new SwupHeadPlugin(),
-        new SwupFormsPlugin()
-    ]
-});
 
 (async function() {
    new Chart(
